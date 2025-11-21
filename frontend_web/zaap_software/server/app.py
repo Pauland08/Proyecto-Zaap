fb9f93b180abe98614b1896ebd22033bd31bd54f
andres859 <126506142+ryazbo@users.noreply.github.com>
2025-11-20 20:55:33 -0500

    api ad

--- frontend_web/zaap_software/server/app.py
+++ frontend_web/zaap_software/server/app.py
@@ -12,3 +12,23 @@ if __name__ == "__main__":
         db.engine.connect()
         print("Conexión a MySQL exitosa.")
     app.run(debug=True, port=8000)
+
+@app.errorhandler(404)
+def bad_request(e):
+    return {"error": "Recurso no encontrado"}, 404
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return {"error": "No autorizado"}, 401
+
+@app.errorhandler(403)
+def forbidden(e):
+    return {"error": "Prohibido"}, 403
+
+@app.errorhandler(404)
+def not_found(e):
+    return {"error": "No encontrado"}, 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return {"error": "Error interno del servidor"}, 500
--- frontend_web/zaap_software/server/config.py
+++ frontend_web/zaap_software/server/config.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 
@@ -8,6 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:tu_contraseña@localhost/tu_basedatos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'clave_secreta_segura'
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
